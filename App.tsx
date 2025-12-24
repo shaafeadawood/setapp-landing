8356f6703b279fa0d73e003d2e7e78eb5865a807
Developer <dev@setapp.com>
2025-12-24 12:21:58 +0500

    refactor: update App.tsx to import all Tailwind-converted components (2025-12-24 14:40)

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import FeatureSection from './components/FeatureSection';
+import VideoSection from './components/VideoSection';
+import Testimonials from './components/Testimonials';
+import PricingBanner from './components/PricingBanner';
+import Footer from './components/Footer';
+import './src/index.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-brand-dark flex flex-col font-sans">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+        <FeatureSection />
+        <VideoSection />
+        <Testimonials />
+        <PricingBanner />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
